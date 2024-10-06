62903c65565756c1d5312900a7e7c9ee27daa7df
rpaul <rpaul93@wgu.edu>
2024-10-06 19:02:25 -0400

    adds default port for postgres

--- db.go
+++ db.go
@@ -2,25 +2,38 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
 	"log"
-
-	_ "modernc.org/sqlite"
+	"os"
 )
 
-func initDB() *sql.DB {
+func initDB() *sql.DB { // Retrieve environment variables
+	host := os.Getenv("POSTGRES_HOST")
+	port := os.Getenv("POSTGRES_PORT")
+	user := os.Getenv("POSTGRES_USER")
+	password := os.Getenv("POSTGRES_PASSWORD")
+	dbname := os.Getenv("POSTGRES_DB")
 
-	db, err := sql.Open("sqlite", "./imageboard.db")
+	if port == "" {
+		port = "5432"
+	}
+
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", connStr)
 	if err != nil {
-		log.Fatal("Failed to connect to database", err)
+		log.Fatal("Failed to connect to the database:", err)
 	}
 
 	query := `
 	CREATE TABLE IF NOT EXISTS posts (
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		username TEXT DEFAULT 'Anonymous',
+		id SERIAL PRIMARY KEY,
+		username VARCHAR(255) DEFAULT 'Anonymous',
 		body TEXT NOT NULL,
-		latitude REAL NOT NULL,
-		longitude REAL NOT NULL,
+		latitude DECIMAL(9,6) NOT NULL,
+		longitude DECIMAL(9,6) NOT NULL,
 		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 	);`
 	_, err = db.Exec(query)
